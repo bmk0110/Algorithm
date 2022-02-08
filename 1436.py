ae26c6f8a65c3312507d14fbc487f94595c4e83e
bmk0110 <bmk0110@naver.com>
2022-02-09 00:02:55 +0900

    20220209

---
+++ 1436.py
@@ -0,0 +1,12 @@
+#1436 영화감독 숌
+N=int(input())
+jongmal=666
+cnt=0
+while True:
+    if '666' in str(jongmal):
+        cnt+=1
+    if cnt == N:
+        break
+    else:
+        jongmal+=1
+print(jongmal)
