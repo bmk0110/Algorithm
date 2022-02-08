ff377ce141ce1af71280c6e0714ea43234030a86
bmk0110 <bmk0110@naver.com>
2022-02-08 22:59:56 +0900

    20220208

--- 7568.py
+++ 7568.py
@@ -1,11 +1,18 @@
-#덩치
+#7568 덩치
 
 N = int(input())
 peoples = []
 for i in range(N):
     a = list(map(int,input().split()))
     peoples.append(a)
+cnt=0
+rank=[]
+for i in range(len(peoples)):
+    cnt=1
+    for j in range(len(peoples)):
+        if peoples[i][0]<peoples[j][0] and peoples[i][1]<peoples[j][1]:
+            cnt+=1
+    rank.append(cnt)
 
-peoples.sort(reverse=True)
-print(peoples)
-
+for i in range(len(rank)):
+    print(rank[i],end=' ')
