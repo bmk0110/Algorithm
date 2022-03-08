3761b8d321da5f3821fe753c39d98be89aa9c699
bmk0110 <bmk0110@naver.com>
2022-03-08 16:47:15 +0900

    220308

--- BOJ/15650.py
+++ BOJ/15650.py
@@ -1,23 +1,30 @@
 #15650 N과 M (2)
 
-def dfs(cnt):
-    if cnt==M:
-        print(*fp)
-    for i in range(N):
-        if check[i]==True:
-            continue
-        check[i]=True
-        fp.append(arr[i])
-        dfs(cnt+1)
-        fp.pop()
-        for j in range(i+1,N):
-            check[j]=False
+# def dfs(cnt):
+#     if cnt==M:
+#         print(*fp)
+#     for i in range(N):
+#         if check[i]==True:
+#             continue
+#         check[i]=True
+#         fp.append(arr[i])
+#         dfs(cnt+1)
+#         fp.pop()
+#         for j in range(i+1,N):
+#             check[j]=False
+#
+#
+# N,M=map(int,input().split())
+# check=[False]*N
+# arr=[i+1 for i in range(N)]
+# fp=[]
+#
+#
+# dfs(0)
 
+from itertools import combinations
 
-N,M=map(int,input().split())
-check=[False]*N
-arr=[i+1 for i in range(N)]
-fp=[]
-
-
-dfs(0)
+N, M = map(int, input().split())
+arr = [i for i in range(1,N+1)]
+for e in list(combinations(arr, M)):
+    print(*e)
