cea1027003f81fdf819352db87837ec53d13709d
bmk0110 <bmk0110@naver.com>
2022-03-11 23:28:50 +0900

    220311

---
+++ 2022 KAKAO BLIND RECRUITMENT/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,23 @@
+import math
+def solution(n, k):
+    tmp = ''
+    while n:
+        tmp += str(n % k)
+        n //= k
+    numb = tmp[::-1]
+    array = []
+    array = list(numb.replace('0', ' ').split())
+    cnt = 0
+    for ar in array:
+        if int(ar) != 1:
+            pr = True
+            for i in range(2, int(math.sqrt(int(ar))) + 1):
+                if int(ar) % i == 0:
+                    pr = False
+                    break
+            if pr == True:
+                cnt += 1
+        else:
+            pass
+
+    return cnt
