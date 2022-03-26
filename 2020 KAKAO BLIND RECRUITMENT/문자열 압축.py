9666f011b681c6c55248cd2879ef0270b1f0ec4b
bmk0110 <bmk0110@naver.com>
2022-03-26 23:43:17 +0900

    20220326

---
+++ 2020 KAKAO BLIND RECRUITMENT/문자열 압축.py
@@ -0,0 +1,28 @@
+def solution(s):
+    arr = [len(s)]
+    for i in range(1,len(s)//2 +1 ):
+        st = ''
+        start=s[0:i]
+        cnt = 1
+
+        for j in range(i,len(s),i):
+            if s[j:j+i] == start:
+                cnt+=1
+            else:
+                if cnt>=2:
+                    st+= str(cnt)+start
+                    start=s[j:j+i]
+                    cnt=1
+                else:
+                    st+=start
+                    start = s[j:j + i]
+        if cnt >= 2:
+            st += str(cnt) + start
+        else:
+            st += start
+        arr.append(len(st))
+    return min(arr)
+
+s="aabbaccc"
+
+print(solution(s))
