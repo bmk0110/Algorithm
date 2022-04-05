a9feac4dcd1b163ca49a7d9b12d7a089ec274a2d
bmk0110 <bmk0110@naver.com>
2022-04-06 00:38:57 +0900

    20220406

--- BOJ/1504.py
+++ BOJ/1504.py
@@ -7,10 +7,7 @@ INF = int(1e9)
 def dijk(start):
     q=[]
     heapq.heappush(q,(0,start))
-    distance = [INF] * (N + 1)
-
     distance[start]=0
-
     while q:
         dist, now = heapq.heappop(q)
         if distance[now]<dist:
@@ -20,21 +17,33 @@ def dijk(start):
             if d<distance[i[1]]:
                 distance[i[1]]=d
                 heapq.heappush(q,(d,i[1]))
-    return distance
+
 N, E = map(int,input().split())
 graph = [[] for _ in range(N+1)]
+distance = [INF]*(N+1)
 for _ in range(E):
     a,b,c= map(int,input().split())
     graph[a].append((c,b))
     graph[b].append((c,a))
-
 point1, point2 = map(int,input().split())
+first=0
+second=0
 
-first = dijk(1)[point1]+dijk(point1)[point2]+dijk(point2)[N]
-second = dijk(1)[point2]+dijk(point2)[point1]+dijk(point1)[N]
+dijk(1)
+first += distance[point1]
+second += distance[point2]
 
+distance = [INF]*(N+1)
+dijk(point1)
+first+=distance[point2]
+second+=distance[N]
 
-if min(first,second)>=INF:
+distance = [INF]*(N+1)
+dijk(point2)
+second+=distance[point1]
+first+=distance[N]
+final= min(first,second)
+if final>=INF:
     print(-1)
 else:
-    print(min(first,second))
+    print(final)
